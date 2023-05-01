bab1896a6ba08dff23357c76a25f0738d32a775e
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-05-01 16:17:08 +0300

    finished

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 const { mnemonic } = require('./secrets.json');
+// In secrets.json file instead of "" we are using secret recovery phrase
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
